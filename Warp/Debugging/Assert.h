b9bf4546a1665819357a2190bbeab5b4616acfc9
Paul Ritaldato <paulritaldato@gmail.com>
2024-09-19 16:11:00 -0400

    random updates to cmake, and improving asserts

--- Warp/Debugging/Assert.h
+++ Warp/Debugging/Assert.h
@@ -1,25 +1,34 @@
 #pragma once
 
+#define WARP_ASSERTIONS_ENABLED
 
-#ifndef NDEBUG
+#ifdef _WIN32
+#include <intrin.h>
+#define DebugBreak() __debugbreak()
+#else
+#define DebugBreak() __builtin_trap()
+#endif
+
+#ifdef WARP_ASSERTIONS_ENABLED
 #include <iostream>
 
 #define DYNAMIC_ASSERT(condition, message) \
-    do { \
-        if(!(condition)){\
-            std::cerr << "\033[1;33mDynamic Assert failure: `" #condition "` Failed in " << __FILE__  \
-                      << " line " << __LINE__ << " with message: " << message << ".\033[0m\n";\
-        }\
-    }while(false)
+    if(condition) {} \
+    else \
+    { \
+     std::cerr << "\033[1;33mDynamic Assert failure: `" #condition "` Failed in " << __FILE__  \
+                      << " line " << __LINE__ << " with message: " << message << ".\033[0m\n"; \   
+        DebugBreak(); \
+    }
 
 #define FATAL_ASSERT(condition, message) \
-    do{ \
-        if(!(condition)){\
-            std::cerr << "\033[1;31mFATAL ASSERT FAILURE: `" #condition "` Failed in " << __FILE__ \
-                      << " line " << __LINE__ << " with message: " << message << ".\033[0m\n";\
-            std::terminate();\
-        }\
-    }while(false)
+    if(condition){} \
+    else \
+    { \ 
+        std::cerr << "\033[1;31mFATAL ASSERT FAILURE: `" #condition "` Failed in " << __FILE__ \
+            << " line " << __LINE__ << " with message: " << message << ".\033[0m\n";\
+        std::terminate();\
+    }
 #else
 #define DYNAMIC_ASSERT(condition, message) do {} while (false)
 #define FATAL_ASSERT(condition, message) do {} while (false)
