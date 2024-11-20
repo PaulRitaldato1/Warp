5826f3a7f7b578ac423935d0efa88ccebe13f01c
Paul Ritaldato <paulritaldato@gmail.com>
2024-11-20 14:06:22 -0500

    Clang Formatting added and a few files are autoformatted as an ex

--- Warp/Engine/Core/WarpEngine.cpp
+++ Warp/Engine/Core/WarpEngine.cpp
@@ -1,81 +1,82 @@
 #include "WarpEngine.h"
 
-#include <Debugging/Logging.h>
 #include <Debugging/Assert.h>
+#include <Debugging/Logging.h>
 #include <UserApplication.h>
 
 #ifdef WARP_WINDOWS
-    #include <Renderer/Platform/Windows/WindowsWindow.h>
+#include <Renderer/Platform/Windows/WindowsWindow.h>
 #elif WARP_LINUX
 #elif WARP_APPLE
 #endif
 
 WarpEngine::WarpEngine(UserApplicationBase* App)
 {
-    FATAL_ASSERT(App != nullptr, "Engine was attempting to init with no UserApplicationBase or WarpEngine::Instance() was called before WarpEngine::Init()");
+	FATAL_ASSERT(App != nullptr, "Engine was attempting to init with no UserApplicationBase or "
+								 "WarpEngine::Instance() was called before WarpEngine::Init()");
 
-    m_timer = GameTimer();
-    LOG_DEBUG("Engine Init Started")
+	m_timer = GameTimer();
+	LOG_DEBUG("Engine Init Started")
 
-    m_app = std::unique_ptr<UserApplicationBase>(App);
+	m_app = std::unique_ptr<UserApplicationBase>(App);
 
 #ifdef WARP_WINDOWS
-    m_window = std::make_unique<WindowsWindow>(App->EngineInitDesc.Name, App->EngineInitDesc.WindowWidth, App->EngineInitDesc.WindowHeight);
-    LOG_DEBUG("Windows Window Initialized");
+	m_window = std::make_unique<WindowsWindow>(App->EngineInitDesc.Name, App->EngineInitDesc.WindowWidth,
+											   App->EngineInitDesc.WindowHeight);
+	LOG_DEBUG("Windows Window Initialized");
 #elif WARP_LINUX
-    LOG_DEBUG("Linux Window Initializing");
+	LOG_DEBUG("Linux Window Initializing");
 #elif WARP_APPLE
-    LOG_DEBUG("Apple Window Initializing");
+	LOG_DEBUG("Apple Window Initializing");
 #endif
 
-    m_bIsRunning = true;
-    m_bIsSuspended = false;
-    m_bResizing = false;
-    m_bIsFullScreen = false;
-    m_lastTime = 0.0f;
+	m_bIsRunning	= true;
+	m_bIsSuspended	= false;
+	m_bResizing		= false;
+	m_bIsFullScreen = false;
+	m_lastTime		= 0.0f;
 
-    FATAL_ASSERT(App->Initialize(), "Application failed to initialize");
-
+	FATAL_ASSERT(App->Initialize(), "Application failed to initialize");
 }
 
 WarpEngine::~WarpEngine()
 {
-    LOG_DEBUG("Shutting down");
+	LOG_DEBUG("Shutting down");
 }
 
 bool WarpEngine::Run()
 {
-    LOG_DEBUG("Engine Run Started");
+	LOG_DEBUG("Engine Run Started");
 
-    m_timer.Reset();
+	m_timer.Reset();
 
-    while(m_bIsRunning)
-    {
-        if(!m_window->PumpMessages())
-        {
-            m_bIsRunning = false;
-        }
+	while (m_bIsRunning)
+	{
+		if (!m_window->PumpMessages())
+		{
+			m_bIsRunning = false;
+		}
 
-        m_timer.Tick();
+		m_timer.Tick();
 
-        if(!m_bIsSuspended)
-        {
-            // LOG_INFO("DeltaTime: " + std::to_string(m_timer.DeltaTime()));
-            //calc frame stats
-            if(!m_app->Update(m_timer.DeltaTime()))
-            {
-                LOG_ERROR("App update failed.");
-                m_bIsRunning = false;
-                break;
-            }
-            //render
-        }
-        else
-        {
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
-        }
-    }
-    m_bIsRunning = false;
+		if (!m_bIsSuspended)
+		{
+			// LOG_INFO("DeltaTime: " + std::to_string(m_timer.DeltaTime()));
+			// calc frame stats
+			if (!m_app->Update(m_timer.DeltaTime()))
+			{
+				LOG_ERROR("App update failed.");
+				m_bIsRunning = false;
+				break;
+			}
+			// render
+		}
+		else
+		{
+			std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		}
+	}
+	m_bIsRunning = false;
 
-    return true;
+	return true;
 }
--- Warp/Engine/Rendering/Renderer/Platform/Windows/WindowsWindow.h
+++ Warp/Engine/Rendering/Renderer/Platform/Windows/WindowsWindow.h
@@ -6,25 +6,29 @@
 
 #include <Windows.h>
 
-LRESULT CALLBACK
-MainWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK MainWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 class WindowsWindow : public IWindow
 {
 public:
+	WindowsWindow(String Name, int width, int height)
+	{
+		Create(Name, width, height);
+	}
 
-    WindowsWindow(String Name, int width, int height) { Create(Name, width, height); }
-    ~WindowsWindow() { Destroy(); }
+	~WindowsWindow()
+	{
+		Destroy();
+	}
 
-    virtual bool Create(String AppName, int width, int height) final;
-    virtual void Destroy() final;
+	virtual bool Create(String AppName, int width, int height) final;
+	virtual void Destroy() final;
 
-    virtual bool PumpMessages() final;
+	virtual bool PumpMessages() final;
 
 private:
-
-    HWND m_wndHnd;
-    HINSTANCE m_appInst;
+	HWND m_wndHnd;
+	HINSTANCE m_appInst;
 };
 
 #endif
