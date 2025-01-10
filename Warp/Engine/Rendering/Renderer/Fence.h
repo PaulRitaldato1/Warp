1597a7856d11d686bc85fb895646d13e69883bea
Paul Ritaldato <paulritaldato@gmail.com>
2025-01-10 11:52:31 -0500

    Last interface

--- Warp/Engine/Rendering/Renderer/Fence.h
+++ Warp/Engine/Rendering/Renderer/Fence.h
@@ -1,1 +1,19 @@
 #pragma once
+
+#include <Common/CommonTypes.h>
+
+class Fence
+{
+public:
+	virtual ~Fence() = default;
+
+	virtual void Initialize() = 0;
+
+	virtual void Wait(u64 Timeout = UINT64_MAX) = 0;
+
+	virtual void Reset() = 0;
+
+	virtual void Signal() = 0;
+
+private:
+};
